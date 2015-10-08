fe058a4f0a9569d655bbbda86e34a6743e5d87f3
T4GAD4 <capi.aurelien@gmail.com>
2015-10-08 13:26:48 +0200

    Adding connexion/deconnexion

--- index.php
+++ index.php
@@ -1,143 +1,42 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <?php include('vues/header.php'); ?>
-    <body>
-        <header id="header">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Livre Audio</h1>
-                </div>
-            </div>
-        </header>
-        <div class="row">
-            <nav>
-                <div class="col-md-2">
-                    <ul>
-                        <li><i class="fa fa-book"></i>Livres audio</li>
-                        <li><i class="fa fa-user"></i>Utilisateur</li>
-                    </ul>
-                </div>
-            </nav>
-            <content>
-                <div class="col-md-10">
-                    <h2>L'ensemble de nos livres audio</h2>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                </div>
-            </content>
-        </div> 
-    </body>
+<html lang="fr">
+
+<!-- EPSI - I4 2015/2016 -->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="description" content="Livre audio">
+    <title>Livre audio</title>
+    
+    <!-- Viewport -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Dosis:300,400,600,700|Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+    <script type="text/javascript" src="assets/js/jquery.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/scripts.js"></script>
+    
+</head>
+<body>
+    <div id="header"></div>
+    <div id="menu"></div>
+    <div id="content"></div>
+    
+</body>
 </html>
+
+<script>
+    
+window.onload= function () {
+    testConnexion();
+};
+
+</script>
--- vues/login.php
+++ vues/login.php
@@ -1,26 +1,35 @@
-<?php
-include('./header.php'); 
-?>
-
-<body style="background:url('./assets/images/background-login.jpg') 50% 50% no-repeat;">
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4" style="margin-top:15%; padding:0 80px;">
-            <form>
-                <h1 style="color:#FFF;text-align:center;">Livre Audio</h1>
-                <div style="padding:15px;">
-                    <input type="text" class="form-control" placeholder="Username" required="" />
-                </div>
-                <div style="padding:15px;">
-                    <input type="password" class="form-control" placeholder="Password" required="" />
-                </div>
-                <div style="text-align:center;">
-                    <a class="btn btn-default submit" href="index.html">Log in</a>
-                </div>
-                <div class="clearfix"></div>
-            </form>
-        </div>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4" style="margin-top:15%; padding:0 80px;">
+        <form id="connexion">
+            <h1 style="color:#FFF;text-align:center;">Livre Audio</h1>
+            <div style="padding:15px;">
+                <input type="email" class="form-control" placeholder="Username" name="mail" required="" value="capi.aurelien@gmail.com" />
+            </div>
+            <div style="padding:15px;">
+                <input type="password" class="form-control" placeholder="Password" name="mdp" required="" value="altec lansing_3" />
+            </div>
+            <div style="text-align:center;">
+                <input type="submit" class="btn btn-default submit" value="Log in"/>
+            </div>
+            <div class="clearfix"></div>
+        </form>
     </div>
-
-</body>
-
-</html>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#connexion').on('submit', function (e) {
+            console.log('test');
+            e.preventDefault();
+            $data = {};
+            $data.mail = $('[name=mail]').val();
+            $data.mdp = $('[name=mdp]').val();
+            $.ajax({
+                url: "utilisateur",
+                method: "POST",
+                data: $data
+            }).done(function () {
+                testConnexion();
+            });
+        });
+    });
+</script>
