bef5ff608c03d53bbfdc0a4a11828f76515312cf
Scraty59 <matthieu.leclerc59@gmail.com>
2015-10-08 15:38:36 +0200

    Css + rating

--- vues/livre.php
+++ vues/livre.php
@@ -58,7 +58,7 @@
             </div>
             <div class="col-md-6">
                 <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                <h3>Test</h3>
+                <h3>Test</h3><div class="rating"><a href="#5" title="Donner 5 étoiles">☆</a><a href="#4" title="Donner 4 étoiles">☆</a><a href="#3" title="Donner 3 étoiles">☆</a><a href="#2" title="Donner 2 étoiles">☆</a><a href="#1" title="Donner 1 étoile">☆</a></div><br>
                 <span class="info">
                     Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
                     Editeur : <span class="editeur"><i>Larousse</i>,</span>
