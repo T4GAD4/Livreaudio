ac230e86660368fd7747bc284b0a8bc965290db8
Scraty59 <matthieu.leclerc59@gmail.com>
2015-10-08 13:46:22 +0200

    Correction style

--- vues/livre.php
+++ vues/livre.php
@@ -1,116 +1,116 @@
 <content>
-                <div class="col-md-10">
-                    <h2>L'ensemble de nos livres audio</h2>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                    <div class="row" style="padding:15px;">
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
-                            <h3>Test</h3>
-                            <span class="info">
-                                Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
-                                Editeur : <span class="editeur"><i>Larousse</i>,</span>
-                                Date : <span class="date"><i>15-12-2014</i></span>
-                            </span>
-                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
-                        </div>
-                    </div>
-                </div>
-            </content>
+    <div class="col-md-10">
+        <h2>L'ensemble de nos livres audio</h2>
+        <div class="row" style="padding:15px;">
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+        </div>
+        <div class="row" style="padding:15px;">
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+        </div>
+        <div class="row" style="padding:15px;">
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+        </div>
+        <div class="row" style="padding:15px;">
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+        </div>
+        <div class="row" style="padding:15px;">
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+            <div class="col-md-6">
+                <img class="couverture" src="assets/images/livre-d-or-spirales.jpg" alt="couverture" />
+                <h3>Test</h3>
+                <span class="info">
+                    Auteur : <span class="auteur"><i>Fred Vargas</i>,</span>
+                    Editeur : <span class="editeur"><i>Larousse</i>,</span>
+                    Date : <span class="date"><i>15-12-2014</i></span>
+                </span>
+                <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi architecto mollitia officia blanditiis, rem eum numquam distinctio eaque iure dicta ipsa animi tenetur totam culpa accusantium tempora, ipsum illum dignissimos!</p>
+            </div>
+        </div>
+    </div>
+</content>
 
