73f37d10c762009b972ebb064a9f0c72921df496
T4GAD4 <capi.aurelien@gmail.com>
2015-10-08 10:58:31 +0200

    Modify WebService

--- WS/WS_WebService.php
+++ WS/WS_WebService.php
@@ -18,7 +18,7 @@ class WS_WebService
 
     }
      
-    private function doGet($id){
+    private function doGet(){
         if($this->get_request_method() != "GET"){ $this->response('',406); }
     }
      
