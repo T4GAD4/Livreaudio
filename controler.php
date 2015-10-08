c48df3a221511836a78ad0195a8a44d37a70d353
Scraty59 <matthieu.leclerc59@gmail.com>
2015-10-08 10:39:46 +0200

    Vues

--- controler.php
+++ controler.php
@@ -1,18 +1,16 @@
 <?php
 
-var_dump($_SERVER['REQUEST_METHOD']);
-
 //création des options du header
 $opts = array(
            'http'=>array(
-                   'method'=>$_SERVER['REQUEST_METHOD'],
-                   'header'=>'Content-type: application/x-www-form-urlencoded',
+                   'method'=>'GET',
+                   'header'=>'Content-type: application/x-www-form-urlencoded'
                 )
 );
 
 //Déclaration du header
 $context = stream_context_create($opts);
- 
+
 //Appel au WebService
 $result = file_get_contents('http://localhost/EPSI/livreaudio/WS/WS_'.$_GET['action'].'.php',false,$context);
 
