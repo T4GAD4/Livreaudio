22ed58216ed2cea84b799f200bb89a959e32dc6e
Scraty59 <matthieu.leclerc59@gmail.com>
2015-10-09 14:24:42 +0200

    Stylisation Header and menu

--- vues/livre.php
+++ vues/livre.php
@@ -1,5 +1,6 @@
 <content>
-    <div class="col-md-10">
+    <div class="col-md-12">
+        <span id="audio"></span>
         <h2 id="titre"></h2>
         <div class="row" style="padding:15px;" id="liste">
         </div>
